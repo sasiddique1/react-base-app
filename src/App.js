facd5248b13507a52be46b8f61d2f22e33d39390
sasiddigue <sasiddique1@gmail.com>
2020-06-09 00:05:16 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My first react project
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App(props) {
 
-export default App;
+      var age = 50;
+
+
+  return (
+    
+    <div>
+      
+    <h1 className="h1"> <span> Hello Pakistan </span> <br></br> from <br></br>
+    <strong className="name">{ props.name} </strong>
+    <br></br>and 
+    <br></br>i am {age-11} years old</h1>
+      
+       
+    <div className="main">
+       
+        <div className="ld">
+       
+        <h3 className="h3" >"Provence/Citys"</h3>
+       
+<ul>
+  <li>Sindh</li>
+  <ul>
+    <li>Karachi</li>
+    <li>Hayderabad</li>
+    <li>Sukkur</li>
+  </ul>
+  <li>Panjab</li>
+  <ul>
+    <li>Lahore</li>
+    <li>Gujranwala</li>
+    <li>Multan</li>
+  </ul>
+  <li>KPK</li>
+  <ul>
+    <li>Peshawer</li>
+    <li>Mardan</li>
+  </ul>
+</ul>
+
+       
+        
+        </div>
+
+
+
+      <div className="pp" >
+
+      <h5 className="h5" >"About Pakistan"</h5>
+      <p  className="p"> Pakistan, officially the Islamic Republic of Pakistan, is a country in South Asia. It is the world's fifth-most populous country with a population exceeding 212.2 million. By area, it is the 33rd-largest country,Pakistan,[b] officially the Islamic Republic of Pakistan,is a country in South Asia. It is the world's fifth-most populous country with a population exceeding 212.2 million. By area, it is the 33rd-largest country,</p>       
+      </div>
+        
+        </div>
+
+        <div className="footer" >Pakista Zindabad</div>
+
+      </div>
+    
+    );
+  }
+  
+  export default App;
+  
